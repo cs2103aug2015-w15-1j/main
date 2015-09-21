8c346ffd1196dc9a25cefea2c757f8a9ce9221da
jun159 <baojun159@gmail.com>
2015-09-22 03:14:01 +0800

    Added CategoryWrapper to represent each category

---
+++ src/main/java/backend/Storage/Task/CategoryWrapper.java
@@ -0,0 +1,32 @@
+package main.java.backend.Storage.Task;
+
+public class CategoryWrapper {
+	
+	private Category category;
+	private String categoryName;
+	
+	public CategoryWrapper() {
+		
+	}
+	
+	public CategoryWrapper(Category category, String categoryName) {
+		setCategory(category);
+		setCategoryName(categoryName);
+	}
+	
+	public Category getCategory() {
+		return category;
+	}
+	
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+}

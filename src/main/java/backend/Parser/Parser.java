ef2e7df9487445ab5f2318b0d5e1cf55578b251b
xaterz <ruofan.yu@gmail.com>
2015-09-23 00:22:27 +0800

    Extended basic test; Added removeExtraTokens test

--- src/main/java/backend/Parser/Parser.java
+++ src/main/java/backend/Parser/Parser.java
@@ -6,43 +6,56 @@ import java.util.Arrays;
 public class Parser {
 	
 	//List of commands accepted by the program
-	private final ArrayList<String> commands = new ArrayList<String>( Arrays.asList("add", "deadline", "description", "priority") );
+	private final ArrayList<String> commands = new ArrayList<String>( 
+											   Arrays.asList("add", "deadline", "description", 
+											   "delete", "done", "event", "priority", "reminder", 
+											   "return", "undo") );
+	private final ArrayList<String> commandsWithoutParameter 
+									= new ArrayList<String>( Arrays.asList("return", "undo") );
 	
 	//The result that will be passed back to logic
-	private ArrayList<String> result = new ArrayList<String>();
+	private ArrayList<String> parsed = new ArrayList<String>();
 	
-	//Temporary holder for the next token that is to be added
-	private String tokenHolder = "";
+	//Temporary holder for the next parameter of result
+	private String parameter = "";
 	
 	/**
-	 * This method parses the user input and returns it as an arraylist 
-	 * @param input passed from logic
-	 * @return parsed input as arraylist
+	 * This method parses the user input and returns it as an arraylist
 	 */
 	public ArrayList<String> parseInput(String input){
+		ArrayList<String> result;
 		String[] inputTokens = input.split(" ");
 		
-		for (String token: inputTokens) {
-			if (commands.contains(token)) { //if token is a command
-				push(tokenHolder);
-				result.add(token);
-			} else {
-				tokenHolder += token + " ";
+		String firstWord = inputTokens[0];
+		if (commandsWithoutParameter.contains(firstWord)) {
+			result = new ArrayList<String>( Arrays.asList(firstWord) );
+		
+		} else {
+			for (String token: inputTokens) {
+				if (commands.contains(token.toLowerCase())) {
+					pushParameter();
+					parsed.add(token.toLowerCase());
+				} else {
+					parameter += token + " ";
+				}
 			}
+			pushParameter();
+	
+			result = new ArrayList<String>(parsed);
+			parsed.clear();
 		}
-		push(tokenHolder);
 		
 		return result;
 	}
 	
 	/**
-	 * This method adds tokenHolder to result and resets tokenHolder (if it is not empty)
+	 * This method adds parameter to result and resets parameter (if it is not empty)
 	 */
-	private void push(String tokenHolder) {
-		if (!tokenHolder.isEmpty()) {
-			tokenHolder = removeEndSpace(tokenHolder);
-			result.add(tokenHolder);
-			tokenHolder = "";
+	private void pushParameter() {
+		if (!parameter.isEmpty()) {
+			parameter = removeEndSpace(parameter);
+			parsed.add(parameter);
+			parameter = "";
 		}
 	}
 
--- src/main/java/backend/Parser/ParserTest.java
+++ src/main/java/backend/Parser/ParserTest.java
@@ -8,14 +8,67 @@ import java.util.Arrays;
 import org.junit.Test;
 
 public class ParserTest {
-	Parser parser = new Parser();
+	public Parser parser = new Parser();
+	public ArrayList<String> parsed; 
+	public ArrayList<String> expected;
 	
 	@Test
 	public void basicTest() {
-	    ArrayList<String> parsed = parser.parseInput("add Project Proposal");
-	    ArrayList<String> expected = new ArrayList<String>( Arrays.asList("add", "Project Proposal") );
+		parsed = parser.parseInput("add Project Proposal");
+	    expected = new ArrayList<String>( Arrays.asList("add", "Project Proposal") );
 	    System.out.println("Expected: " + expected.toString());
-	    System.out.println("Actual: " + parsed.toString());
+	    System.out.println("Actual:   " + parsed.toString());
+	    assertEquals(expected, parsed);
+	    
+	    parsed = parser.parseInput("deadline 30 October 2359");
+	    expected = new ArrayList<String>( Arrays.asList("deadline", "30 October 2359") );
+	    System.out.println("Expected: " + expected.toString());
+	    System.out.println("Actual:   " + parsed.toString());
+	    assertEquals(expected, parsed);
+	    
+	    parsed = parser.parseInput("priority 5");
+	    expected = new ArrayList<String>( Arrays.asList("priority", "5") );
+	    System.out.println("Expected: " + expected.toString());
+	    System.out.println("Actual:   " + parsed.toString());
+	    assertEquals(expected, parsed);
+	    
+	    parsed = parser.parseInput("description i need to start doing this!");
+	    expected = new ArrayList<String>( Arrays.asList("description", "i need to start doing this!") );
+	    System.out.println("Expected: " + expected.toString());
+	    System.out.println("Actual:   " + parsed.toString());
+	    assertEquals(expected, parsed);
+	    
+		parsed = parser.parseInput("ADD PROJECT PROPOSAL");
+	    expected = new ArrayList<String>( Arrays.asList("add", "PROJECT PROPOSAL") );
+	    System.out.println("Expected: " + expected.toString());
+	    System.out.println("Actual:   " + parsed.toString());
+	    assertEquals(expected, parsed);
+	    
+	    parsed = parser.parseInput("done Project Proposal");
+	    expected = new ArrayList<String>( Arrays.asList("done", "Project Proposal") );
+	    System.out.println("Expected: " + expected.toString());
+	    System.out.println("Actual:   " + parsed.toString());
+	    assertEquals(expected, parsed);
+	    
+	    parsed = parser.parseInput("return");
+	    expected = new ArrayList<String>( Arrays.asList("return") );
+	    System.out.println("Expected: " + expected.toString());
+	    System.out.println("Actual:   " + parsed.toString());
+	    assertEquals(expected, parsed);
+	}
+	
+	@Test
+	public void removeExtraTokens() {
+	    parsed = parser.parseInput("return extra extra extra words");
+	    expected = new ArrayList<String>( Arrays.asList("return") );
+	    System.out.println("Expected: " + expected.toString());
+	    System.out.println("Actual:   " + parsed.toString());
+	    assertEquals(expected, parsed);
+	    
+	    parsed = parser.parseInput("undo extra extra extra words");
+	    expected = new ArrayList<String>( Arrays.asList("undo") );
+	    System.out.println("Expected: " + expected.toString());
+	    System.out.println("Actual:   " + parsed.toString());
 	    assertEquals(expected, parsed);
 	}
 }

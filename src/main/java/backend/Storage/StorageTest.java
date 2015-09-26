7641c2e7338e0fcc6746aa5196ef03816c2752a2
jun159 <baojun159@gmail.com>
2015-09-27 06:18:55 +0800

    Added testcases to test for addCategory() method

--- src/main/java/backend/Storage/StorageTest.java
+++ src/main/java/backend/Storage/StorageTest.java
@@ -8,6 +8,27 @@ import org.junit.Before;
 
 public class StorageTest {
 	
+	/* ======================== Categories =========================== */
+	private static final String CATEGORY1 = "CS2102";
+	private static final String CATEGORY2 = "CS2103";
+	private static final String CATEGORY3 = "Personal";
+	
+	/* ======================== CS2102 tasks ========================= */
+	private static final String CATEGORY1_TASK1 = "Read up on PHP";
+	private static final String CATEGORY1_TASK2 = "Revise nested queries";
+	
+	/* ======================== CS2103 tasks ========================= */
+	private static final String CATEGORY2_TASK1 = "TextBuddy CE2";
+	private static final String CATEGORY2_TASK2 = "Post lecture quiz 5";
+	private static final String CATEGORY2_TASK3 = "Tutorial 5";
+	private static final String CATEGORY2_FLOAT1 = "Watch webcast";
+	
+	/* ======================== Personal tasks ========================= */
+	private static final String CATEGORY3_TASK1 = "Help mum buy groceries";
+	private static final String CATEGORY3_FLOAT1 = "Get haircut someday";
+	private static final String CATEGORY3_EVENT1 = "Cycling @ East Coast";
+	private static final String CATEGORY3_EVENT2 = "3D'09 gathering @ Mr Teo's house";
+	
 	private static final String TEST_FILE_NAME = "test.txt";
 	
 	Storage storage;

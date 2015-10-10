56a44725527f50b7054fb5a5678d4175346eee8a
jun159 <baojun159@gmail.com>
2015-10-10 08:55:10 +0800

    Implemented Comparable methods to sort priority and deadline

--- src/main/java/backend/Sorter/Sorter.java
+++ src/main/java/backend/Sorter/Sorter.java
@@ -2,6 +2,7 @@ package main.java.backend.Sorter;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 import main.java.backend.Storage.Task.Category;
 import main.java.backend.Storage.Task.Task;
@@ -20,14 +21,38 @@ public class Sorter {
 		}
 		return sortedCurrentState;
 	}
+	
+	private Comparator<Task> sortPriority = new Comparator<Task> () {
+		public int compare(Task left, Task right) {
+			if(left.getPriority() < right.getPriority()) {
+				return -1;
+			} else if(left.getPriority() > right.getPriority()) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
+	};
+	
+	private Comparator<Task> sortDeadline = new Comparator<Task> () {
+		public int compare(Task left, Task right) {
+			if(left.getEndTime() < right.getEndTime()) {
+				return -1;
+			} else if(left.getEndTime() > right.getEndTime()) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
+	};
 
 	private ArrayList<Task> sortDate(ArrayList<Task> currentState2) {
-		Collections.sort(currentState2, Task.sortPriority);
+		Collections.sort(currentState2, sortPriority);
 		return currentState2;
 	}
 
 	private ArrayList<Task> sortPriority(ArrayList<Task> currentState2) {
-		Collections.sort(currentState2, Task.sortDeadline);
+		Collections.sort(currentState2, sortDeadline);
 		return currentState2;
 	}
 

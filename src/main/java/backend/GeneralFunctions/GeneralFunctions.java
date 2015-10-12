c71ad3b9cdfa611b4da51e8f7648fe1ab0d5600f
jun159 <baojun159@gmail.com>
2015-10-13 02:58:51 +0800

    Prevent GeneralFunctions from being instantiated

--- src/main/java/backend/GeneralFunctions/GeneralFunctions.java
+++ src/main/java/backend/GeneralFunctions/GeneralFunctions.java
@@ -6,33 +6,37 @@ import java.util.Date;
 import main.java.backend.Storage.Task.Task;
 
 public class GeneralFunctions {
-	
-	private static final SimpleDateFormat formatterForDateTime = 
+
+	public static final SimpleDateFormat formatterForDateTime = 
 			new SimpleDateFormat("EEE, dd MMM hh:mma");
-	
+	public static final SimpleDateFormat standardFormat = 
+			new SimpleDateFormat("EEE, dd MMM hh:mma yyyy");
+
+	private GeneralFunctions() {
+		// prevent GeneralFunctions.java from being instantiated
+	}
+
 	public static long stringToMillisecond(String dateTime) {
-        try {
-            Date tempDateTime = formatterForDateTime.parse(dateTime);
-            long dateTimeMillisecond = tempDateTime.getTime();
-            return (dateTimeMillisecond);
-        } catch (java.text.ParseException e) {
+		try {
+			Date tempDateTime = formatterForDateTime.parse(dateTime);
+			long dateTimeMillisecond = tempDateTime.getTime();
+			return (dateTimeMillisecond);
+		} catch (java.text.ParseException e) {
 			e.printStackTrace();
 		}
 
-        //Should not reach here
-        return -1;
-    }
-	
-	
+		//Should not reach here
+		return -1;
+	}
+
 	public static long getCurrentTime() {
-		
+
 		long currentMilliseconds = System.currentTimeMillis();
-		SimpleDateFormat standardFormat = new SimpleDateFormat("EEE, dd MMM hh:mma yyyy");
 		Date resultdate = new Date(currentMilliseconds);
-		
+
 		return GeneralFunctions.stringToMillisecond(standardFormat.format(resultdate));
-    }
-	
+	}
+
 	public static long getTime(Task task) {
 
 		if(task.getEndTime() > 0) {

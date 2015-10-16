0b9a77fab37d9578ba675c766f6bc66a14ea6cc3
jun159 <baojun159@gmail.com>
2015-10-16 10:26:13 +0800

    Added abstract class for Storage

--- src/main/java/backend/Storage/StorageOperation.java
+++ src/main/java/backend/Storage/StorageOperation.java
@@ -7,10 +7,10 @@ import main.java.backend.Storage.Task.Task;
 
 public abstract class StorageOperation {
 
-	public static final String DEFAULT_DESKTOP_LOCATION = 
-			System.getProperty("user.home") + "/Desktop";
-	public static final String DEFAULT_FILE_LOCATION = 
-			DEFAULT_DESKTOP_LOCATION + "/filename.txt";
+	public static final String DEFAULT_PATH_LOCATION = System.getProperty("user.home") + "/Desktop";
+	public static final String DEFAULT_FILE_NAME = "/filename.txt";
+	public static final String DEFAULT_FILE_LOCATION = DEFAULT_PATH_LOCATION + DEFAULT_FILE_NAME;
+	
 	public String CURRENT_FILE_LOCATION = new String();
 	public File textFile;
 

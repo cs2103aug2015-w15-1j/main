1bb8445a21e1017f67f95b4f5ed9a185355c0c63
jun159 <baojun159@gmail.com>
2015-11-04 07:10:24 +0800

    Rename filepath when necessary

---
+++ src/main/java/backend/Storage/StorageFilePath.java
@@ -0,0 +1,74 @@
+package main.java.backend.Storage;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Properties;
+
+public class StorageFilePath {
+
+	private static final String ERROR_READ_DATA = "An error occured when retrieving data from config file.";
+	private static final String ERROR_WRITE_DATA = "An error occured when writing data from config file.";
+	
+	private static final String FILE_CONFIGURATION = "config.properties";
+	private static final String FILE_HEADING = "File path for data storage";
+	private static final String FILE_KEY = "filepath";
+	
+	// TODO: Change default file path
+	private static final String PATH_LOCATION_DEFAULT = System.getProperty("user.home") + "/Desktop" + "/filename.txt";
+	private static String PATH_LOCATION_OLD;
+	private static String PATH_LOCATION_NEW;
+	
+	private File textFile;
+	private FileReader reader;
+	private FileWriter writer;
+	
+	private Properties properties;
+
+	public StorageFilePath() {
+		
+		properties = new Properties();
+		PATH_LOCATION_OLD = retrieveFilePath();
+		textFile = new File(PATH_LOCATION_OLD);
+	}
+	
+	private String retrieveFilePath() {
+		
+		String filePath = properties.getProperty(FILE_KEY);
+		
+		if(filePath == null) {
+			return PATH_LOCATION_DEFAULT;
+		} else {
+			return filePath;
+		}
+	}
+
+	public void execute(String newFilePath) {
+		
+		try {
+			writer = new FileWriter(FILE_CONFIGURATION);
+			textFile.renameTo(new File(newFilePath));
+			properties.setProperty(FILE_KEY, newFilePath);
+			properties.store(writer, FILE_HEADING);
+			writer.close();
+		} catch (IOException e) {
+			System.out.println(ERROR_WRITE_DATA);
+		}	
+	}
+	
+	public String retrieve() {
+		
+		try {
+			reader = new FileReader(FILE_CONFIGURATION);
+			properties.load(reader);
+		} catch (Exception e) {
+			System.out.println(ERROR_READ_DATA);
+		}
+		
+		System.out.println("RETRIEVE: " + retrieveFilePath());
+		return retrieveFilePath();
+	}
+
+}

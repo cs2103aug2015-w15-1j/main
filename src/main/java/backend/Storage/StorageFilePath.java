cf346bc33b9b41df95c2974cf0fac8b88b903774
jun159 <baojun159@gmail.com>
2015-11-05 05:46:27 +0800

    Ensure filepath exist before transferring data to the new filepath

--- src/main/java/backend/Storage/StorageFilePath.java
+++ src/main/java/backend/Storage/StorageFilePath.java
@@ -5,7 +5,6 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -15,18 +14,20 @@ import java.util.Properties;
 public class StorageFilePath {
 
 	private static final String ERROR_READ_DATA = "An error occured when retrieving data from config file.";
+	private static final String ERROR_TRANSFER_DATA = "An error occured when transfering data from config file.";
 	private static final String ERROR_WRITE_DATA = "An error occured when writing data from config file.";
 	
 	private static final String FILE_CONFIGURATION = "config.properties";
 	private static final String FILE_HEADING = "File path for data storage";
 	private static final String FILE_KEY = "filepath";
 	
-	private String filePathOld, filePathNew = "";
+	private static final String FRONTSLASH = "/";
+	private static final String BACKSLASH_1 = "\\";
+	private static final String BACKSLASH_2 = "\\\\";
 	
 	// TODO: Change default file path
 	private static final String PATH_LOCATION_DEFAULT = System.getProperty("user.home") + "/Desktop" + "/filename.txt";
 	
-	private File textFile;
 	private FileReader reader;
 	private FileWriter writer;
 	
@@ -35,18 +36,17 @@ public class StorageFilePath {
 	public StorageFilePath() {
 		
 		properties = new Properties();
-		textFile = new File(retrieveFilePath());
 	}
 	
 	
 	//@@author A0121284N
-	private void dataTransfer() {
+	private void dataTransfer(String newFilePath) {
 		String oldFilePath = retrieveFilePath();
 		byte[] buffer = new byte[10000];
 		try {
 			FileInputStream fileInput = new FileInputStream(oldFilePath);
 			BufferedInputStream bufferedInput = new BufferedInputStream(fileInput);
-			FileOutputStream fileOutput = new FileOutputStream(this.filePathNew);
+			FileOutputStream fileOutput = new FileOutputStream(newFilePath);
 			BufferedOutputStream bufferedOutput = new BufferedOutputStream(fileOutput);
 			while(true) {
 				int length = fileInput.read(buffer);
@@ -62,13 +62,41 @@ public class StorageFilePath {
 			bufferedInput.close();
 			bufferedOutput.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			System.out.println(ERROR_TRANSFER_DATA);
 		}
 		
 	}
 	
 	//@@author A0126258A
+	private String removeFileName(String[] tokenize, String slash) {
+		
+		String filePath = new String();
+		int size = tokenize.length - 1;
+		
+		for(int i = 0; i < size; i++) {
+			filePath += slash + tokenize[i];
+		}
+		
+		return filePath;
+	}
+	
+	private boolean isFilePathExist(String filePath) {
+		
+		filePath = filePath.replace("\\", "\\\\");
+		String tokenizeFrontSlash = removeFileName(filePath.split(FRONTSLASH), FRONTSLASH);
+		String tokenizeBackSlash = removeFileName(filePath.split(BACKSLASH_2), BACKSLASH_1).replaceAll("\\\\+","\\\\");
+		
+		File filePathMac = new File(tokenizeFrontSlash);
+		File filePathWindows = new File(tokenizeBackSlash);
+		
+		if (filePathMac.exists() || filePathWindows.exists()){
+		    return true;
+		} else {
+			return false;
+		}
+	}
+	
 	private String retrieveFilePath() {
 		
 		String filePath = properties.getProperty(FILE_KEY);
@@ -81,21 +109,18 @@ public class StorageFilePath {
 	}
 
 	public void execute(String newFilePath) {
-		
-		filePathOld = retrieveFilePath();
-		filePathNew = newFilePath;
-		
-		try {
-			writer = new FileWriter(FILE_CONFIGURATION);
-			textFile.renameTo(new File(newFilePath));
-			dataTransfer();
-			properties.setProperty(FILE_KEY, newFilePath);
-			properties.store(writer, FILE_HEADING);
-			writer.close();
-		} catch (IOException e) {
-			properties.setProperty(FILE_KEY, filePathOld);
-			System.out.println(ERROR_WRITE_DATA);
-		}	
+
+		if(newFilePath.contains(".txt") && isFilePathExist(newFilePath)) {
+			try {
+				writer = new FileWriter(FILE_CONFIGURATION);
+				dataTransfer(newFilePath);
+				properties.setProperty(FILE_KEY, newFilePath);
+				properties.store(writer, FILE_HEADING);
+				writer.close();
+			} catch (IOException e) {
+				System.out.println(ERROR_WRITE_DATA);
+			}	
+		}
 	}
 
 	public String retrieve() {
@@ -107,7 +132,6 @@ public class StorageFilePath {
 			System.out.println(ERROR_READ_DATA);
 		}
 		
-		System.out.println("RETRIEVE: " + retrieveFilePath());
 		return retrieveFilePath();
 	}
 

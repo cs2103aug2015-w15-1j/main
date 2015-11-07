6f1e57d13e6b0f9fb3474fd6f5717a8ee391f9d1
jun159 <baojun159@gmail.com>
2015-11-08 02:37:00 +0800

    Remove magic strings in FilePathCommand

--- src/main/java/backend/Logic/FilePathCommand.java
+++ src/main/java/backend/Logic/FilePathCommand.java
@@ -4,10 +4,15 @@ package main.java.backend.Logic;
 import main.java.backend.Storage.Storage;
 
 public class FilePathCommand extends Command {
-	String filepath = "";
-	Storage storageComponent;
+	
+	private static final String EXECUTION_FILEPATH_SUCCESSFUL = "File path is updated successfully.";
+	private static final String EXECUTION_FILEPATH_UNSUCCESSFUL = "Invalid file path. Please try again";
+	
+	private String filepath;
+	private Storage storageComponent;
 
 	public FilePathCommand(Type typeInput, Storage storage) {
+		
 		super(typeInput);
 		storageComponent = storage;
 	}
@@ -21,11 +26,13 @@ public class FilePathCommand extends Command {
 	}
 	
 	public String execute() {
-		Boolean isExecuted = storageComponent.updateFilePath(filepath);
+		
+		boolean isExecuted = storageComponent.updateFilePath(filepath);
+		
 		if (isExecuted) {
-			return "File path changed to "+filepath;
+			return EXECUTION_FILEPATH_SUCCESSFUL;
 		} else {
-			return "File path does not exist";
+			return EXECUTION_FILEPATH_UNSUCCESSFUL;
 		}
 		
 	}
